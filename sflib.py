650942842219e7058e3a80acc5f81a8f08329128
smicallef <steve@binarypool.com>
2013-10-12 21:26:29 +0200

    Formatting, TLD parsing, Caching.

--- sflib.py
+++ sflib.py
@@ -147,6 +147,41 @@ class SpiderFoot:
 
         return os.path.dirname(unicode(__file__, sys.getfilesystemencoding()))
 
+    #
+    # Caching
+    #
+
+    # Return the cache path
+    def cachePath(self):
+        return self.myPath() + '/cache'
+
+    # Store data to the cache
+    def cachePut(self, label, data):
+        cacheFile = self.cachePath() + "/" + label
+        fp = file(cacheFile, "w")
+        if type(data) is list:
+            for line in data:
+                fp.write(line + '\n')
+        else:
+            fp.write(data)
+        fp.close()
+
+    # Retreive data from the cache
+    def cacheGet(self, label, timeoutHrs):
+        cacheFile = self.cachePath() + "/" + label
+        try:
+            (m, i, d, n, u, g, sz, atime, mtime, ctime) = os.stat(cacheFile)
+        
+            if mtime > time.time() - timeoutHrs*3600:
+                fp = file(cacheFile, "r")
+                fileContents = fp.read()
+                fp.close()
+
+                return fileContents
+            else:
+                return None
+        except BaseException as e:
+            return None
 
     #
     # Configuration process
@@ -715,3 +750,109 @@ class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
             self, req, fp, code, msg, headers)
         return result
 
+
+"""
+Public Suffix List module for Python.
+See LICENSE.tp for applicable license.
+"""
+
+class PublicSuffixList(object):
+	def __init__(self, input_data):
+		"""Reads and parses public suffix list.
+		
+		input_file is a file object or another iterable that returns
+		lines of a public suffix list file. If input_file is None, an
+		UTF-8 encoded file named "publicsuffix.txt" in the same
+		directory as this Python module is used.
+		
+		The file format is described at http://publicsuffix.org/list/
+		"""
+
+		#if input_file is None:
+			#input_path = os.path.join(os.path.dirname(__file__), 'publicsuffix.txt')
+			#input_file = codecs.open(input_path, "r", "utf8")
+
+		root = self._build_structure(input_file)
+		self.root = self._simplify(root)
+
+	def _find_node(self, parent, parts):
+		if not parts:
+			return parent
+
+		if len(parent) == 1:
+			parent.append({})
+
+		assert len(parent) == 2
+		negate, children = parent
+
+		child = parts.pop()
+
+		child_node = children.get(child, None)
+
+		if not child_node:
+			children[child] = child_node = [0]
+
+		return self._find_node(child_node, parts)
+
+	def _add_rule(self, root, rule):
+		if rule.startswith('!'):
+			negate = 1
+			rule = rule[1:]
+		else:
+			negate = 0
+
+		parts = rule.split('.')
+		self._find_node(root, parts)[0] = negate
+
+	def _simplify(self, node):
+		if len(node) == 1:
+			return node[0]
+
+		return (node[0], dict((k, self._simplify(v)) for (k, v) in node[1].items()))
+
+	def _build_structure(self, fp):
+		root = [0]
+
+		for line in fp:
+			line = line.strip()
+			if line.startswith('//') or not line:
+				continue
+
+			self._add_rule(root, line.split()[0].lstrip('.'))
+
+		return root
+
+	def _lookup_node(self, matches, depth, parent, parts):
+		if parent in (0, 1):
+			negate = parent
+			children = None
+		else:
+			negate, children = parent
+
+		matches[-depth] = negate
+
+		if depth < len(parts) and children:
+			for name in ('*', parts[-depth]):
+				child = children.get(name, None)
+				if child is not None:
+					self._lookup_node(matches, depth+1, child, parts)
+
+	def get_public_suffix(self, domain):
+		"""get_public_suffix("www.example.com") -> "example.com"
+
+		Calling this function with a DNS name will return the
+		public suffix for that name.
+
+		Note that for internationalized domains the list at
+		http://publicsuffix.org uses decoded names, so it is
+		up to the caller to decode any Punycode-encoded names.
+		"""
+
+		parts = domain.lower().lstrip('.').split('.')
+		hits = [None] * len(parts)
+
+		self._lookup_node(hits, 1, self.root, parts)
+
+		for i, what in enumerate(hits):
+			if what is not None and what == 0:
+				return '.'.join(parts[i:])
