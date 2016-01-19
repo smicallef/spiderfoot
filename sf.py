d729eafd6931e369a093879ab6468f7b0d20a44a
smicallef <steve@binarypool.com>
2016-01-19 20:28:34 +0100

    Added GEXF as checked library.

--- sf.py
+++ sf.py
@@ -21,7 +21,7 @@ if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
 deps = ['M2Crypto', 'netaddr', 'dns', 'cherrypy', 'mako', 'socks',
-        'pyPdf', 'metapdf', 'openxmllib', 'stem', 'bs4']
+        'pyPdf', 'metapdf', 'openxmllib', 'stem', 'bs4', 'gexf']
 for mod in deps:
     try:
         if mod.startswith("ext."):
