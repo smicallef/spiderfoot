61d8febaa8699545ca1ed2e2efa42f48151ac952
Steve <steve@binarypool.com>
2016-10-31 13:33:39 -0700

    Added dependencies.

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
         "py2exe": {
             "packages": ["modules", "ext.dns", "sflib", "sfwebui", "sfdb", "mako",
                          "cherrypy", "M2Crypto", "netaddr", "ext.socks", "ext.pyPdf",
-                         "ext.metapdf", "ext.openxmllib", "ext.stem"],
+                         "ext.metapdf", "ext.openxmllib", "ext.stem",
+                         "ext.phonenumbers", "ext.gexf", "ext.bs4" ],
             "bundle_files": 1,
             "compressed": True,
             "includes": ['lxml._elementpath']
