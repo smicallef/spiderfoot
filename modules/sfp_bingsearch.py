297d73e181500f4a4bc5ab29a27cc6dab8f6a52e
Amr Awad <amro.khir@gmail.com>
2019-08-27 06:11:41 +0200

    feat: update sfp_bingsearch module to use bing api

--- modules/sfp_bingsearch.py
+++ modules/sfp_bingsearch.py
@@ -9,25 +9,18 @@
 # Copyright:   (c) Steve Micallef 2013
 # Licence:     GPL
 # -------------------------------------------------------------------------------
-
+import requests
 from sflib import SpiderFoot, SpiderFootPlugin, SpiderFootEvent
 
 
 class sfp_bingsearch(SpiderFootPlugin):
     """Bing:Footprint,Investigate,Passive:Search Engines::Some light Bing scraping to identify sub-domains and links."""
 
-
-
-
     # Default options
-    opts = {
-        'pages': 20  # Number of bing results pages to iterate
-    }
+    opts = {"pages": 20}  # Number of max bing results to request from api
 
     # Option descriptions
-    optdescs = {
-        'pages': "Number of Bing results pages to iterate through."
-    }
+    optdescs = {"pages": "Number of max bing results to request from api."}
 
     results = list()
 
@@ -59,42 +52,57 @@ class sfp_bingsearch(SpiderFootPlugin):
             self.results.append(eventData)
 
         # Sites hosted on the domain
-        pages = self.sf.bingIterate("site:" + eventData, dict(limit=self.opts['pages'],
-                                                              useragent=self.opts['_useragent'],
-                                                              timeout=self.opts['_fetchtimeout']))
-        if pages is None:
-            self.sf.info("No results returned from Bing.")
-            return None
+        endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/search"
+        # ToDO: figure out how to add credentials in config:
+        key = ""
+        response = requests.get(
+            endpoint,
+            timeout=self.opts["_fetchtimeout"],
+            headers={
+                "Ocp-Apim-Subscription-Key": key,
+                "User-Agent": self.opts["_useragent"],
+            },
+            params={
+                "q": "site:" + eventData,
+                "responseFilter": "Webpages",
+                "count": self.opts["pages"],
+            },
+        )
+        if response.status_code != 200:
+            message = "Failed to talk to Bing API, status code: %s, response: %s".format(
+                response.status_code, response.text
+            )
+            self.error(message)
 
-        new_pages = list(set(pages.keys()) - set(self.results))
+        links_in_page = [
+            result["url"] for result in response.json()["webPages"]["value"]
+        ]
+        new_links = list(set(links_in_page) - set(self.results))
 
-        # Add new pages to results
-        self.results.extend(new_pages)
+        # Add new links to results
+        self.results.extend(new_links)
 
-        for page in new_pages:
-            # Allow gracefully stopping the scan
-            if self.checkForStop():
-                return None
+        internal_links = [
+            link for link in new_links if self.sf.urlFQDN(link).endswith(eventData)
+        ]
+        for link in internal_links:
+            self.sf.debug("Found a link: " + link)
 
-            links_in_page = self.sf.parseLinks(page, pages[page], eventData)
-            new_links = list(set(links_in_page) - set(self.results))
+            evt = SpiderFootEvent("LINKED_URL_INTERNAL", link, self.__name__, event)
+            self.notifyListeners(evt)
 
-            # Add new links to results
-            self.results.extend(new_links)
-
-            internal_links = [link for link in new_links if self.sf.urlFQDN(link).endswith(eventData)]
-            for link in internal_links:
-                self.sf.debug("Found a link: " + link)
-
-                evt = SpiderFootEvent("LINKED_URL_INTERNAL", link,
-                                        self.__name__, event)
-                self.notifyListeners(evt)
-
-            if internal_links:
-                # Submit the bing results for analysis
-                evt = SpiderFootEvent("SEARCH_ENGINE_WEB_CONTENT", pages[page],
-                                      self.__name__, event)
-                self.notifyListeners(evt)
+        if internal_links:
+            # Submit the bing results for analysis
+            bingsearch_url = response.json()["webPages"]["webSearchUrl"]
+            response = self.sf.fetchUrl(
+                bingsearch_url,
+                timeout=self.opts["_fetchtimeout"],
+                useragent=self.opts["_useragent"],
+            )
+            evt = SpiderFootEvent(
+                "SEARCH_ENGINE_WEB_CONTENT", response["content"], self.__name__, event
+            )
+            self.notifyListeners(evt)
 
 
 # End of sfp_bingsearch class
