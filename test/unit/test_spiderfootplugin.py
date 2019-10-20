e6ab5cdc9da0326fa8243c4564c4c27a88e15873
Brendan Coles <bcoles@gmail.com>
2019-10-20 09:03:32 +0000

    Add tests

---
+++ test/unit/test_spiderfootplugin.py
@@ -0,0 +1,19 @@
+# test_spiderfootplugin.py
+import sflib
+from sflib import SpiderFootPlugin
+import unittest
+ 
+class TestSpiderFootPlugin(unittest.TestCase):
+    """
+    Test SpiderFoot
+    """
+ 
+    def test_init(self):
+        """
+        Test __init__(self)
+        """
+        self.assertEqual('TBD', 'TBD')
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ test/unit/test_spiderfootscanstatus.py
@@ -0,0 +1,29 @@
+# test_spiderfootscanstatus.py
+import unittest
+
+class SpiderFootScanStatus(unittest.TestCase):
+    """
+    Test SpiderFootScanStatus
+    """
+
+    def test_set_status(self):
+        """
+        Test setStatus(self, scanId, status)
+        """
+        self.assertEqual('TBD', 'TBD')
+
+    def test_get_status(self):
+        """
+        Test getStatus(self, scanId)
+        """
+        self.assertEqual('TBD', 'TBD')
+
+    def test_get_status_all(self):
+        """
+        Test getStatusAll(self)
+        """
+        self.assertEqual('TBD', 'TBD')
+
+if __name__ == '__main__':
+    unittest.main()
+
