53f98ba4d75a21ae1032e29fd289d40eef9428f2
smicallef <steve@binarypool.com>
2019-11-19 22:13:15 +0100

    Bug fix.

--- modules/sfp_flickr.py
+++ modules/sfp_flickr.py
@@ -146,7 +146,7 @@ class sfp_flickr(SpiderFootPlugin):
 
             # Extract emails
             for photo in data['photos']['photo']:
-                emails = self.sf.parseEmails(bytes(str(photo), "ascii").decode('unicode-escape'))
+                emails = self.sf.parseEmails(str(bytes(photo).decode('unicode-escape')))
                 for email in emails:
                     # Skip unrelated emails
                     mailDom = email.lower().split('@')[1]
