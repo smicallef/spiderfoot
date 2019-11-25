b49b9a950159fb7f452a2de96c7acab87082aae7
smicallef <steve@binarypool.com>
2019-11-25 18:49:05 +0100

    Increase timeout.

--- modules/sfp_crt.py
+++ modules/sfp_crt.py
@@ -67,7 +67,7 @@ class sfp_crt(SpiderFootPlugin):
         }
 
         res = self.sf.fetchUrl('https://crt.sh/?' + urllib.parse.urlencode(params),
-                               timeout=self.opts['_fetchtimeout'],
+                               timeout=15,
                                useragent=self.opts['_useragent'])
 
         if res['content'] is None:
@@ -136,7 +136,7 @@ class sfp_crt(SpiderFootPlugin):
             }
 
             res = self.sf.fetchUrl('https://crt.sh/?' + urllib.parse.urlencode(params),
-                                   timeout=self.opts['_fetchtimeout'],
+                                   timeout=15,
                                    useragent=self.opts['_useragent'])
 
             try:
