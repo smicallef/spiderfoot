e05c7ddc4dd79a617e5085e5784f3a5f1f2b54ec
Krishnasis Mandal <krishnasismandal97@gmail.com>
2020-04-29 23:41:49 +0530

    Updated date in comment

--- modules/sfp_countryname.py
+++ modules/sfp_countryname.py
@@ -7,7 +7,7 @@
 # Author:      Krishnasis Mandal <krishnasis@hotmail.com>
 #
 # Created:     28/04/2020
-# Copyright:   (c) Steve Micallef 2012
+# Copyright:   (c) Steve Micallef
 # Licence:     GPL
 # -------------------------------------------------------------------------------
 
