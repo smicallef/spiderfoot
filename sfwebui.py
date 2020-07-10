4f36f971cca909e6604cbdca786685fdabb07b79
Brendan Coles <bcoles@gmail.com>
2020-07-10 20:16:56 +0000

    Remove inline styles

--- sfwebui.py
+++ sfwebui.py
@@ -61,7 +61,7 @@ class SpiderFootWebUi:
           'request.error_response': self.error_page
         })
 
-        secure_headers = SecureHeaders(csp="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'")
+        secure_headers = SecureHeaders(csp="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self'")
 
         cherrypy.config.update({
             "tools.response_headers.on": True,
