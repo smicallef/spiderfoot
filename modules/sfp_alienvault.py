b827987a202ee05637640cf9d15495521709a583
Krishnasis Mandal <krishnasismandal97@gmail.com>
2020-08-17 10:08:08 +0530

    Spacing

--- modules/sfp_alienvault.py
+++ modules/sfp_alienvault.py
@@ -18,6 +18,7 @@ from sflib import SpiderFoot, SpiderFootPlugin, SpiderFootEvent
 
 class sfp_alienvault(SpiderFootPlugin):
     """AlienVault OTX:Investigate,Passive:Reputation Systems:apikey:Obtain information from AlienVault Open Threat Exchange (OTX)"""
+        
         meta = {
         'name': "AlienVault OTX",
         'summary': "Obtain information from AlienVault Open Threat Exchange (OTX)",
