85373da5bae9cba6e2630d0d1c5dc0c5e10e4816
Krishnasis Mandal <krishnasismandal97@gmail.com>
2020-08-18 20:59:03 +0530

    Update fringeproject meta

--- modules/sfp_fringeproject.py
+++ modules/sfp_fringeproject.py
@@ -25,7 +25,20 @@ class sfp_fringeproject(SpiderFootPlugin):
 		'summary': "Obtain network information from Fringe Project API.",
 		'flags': [ "" ],
 		'useCases': [ "Investigate", "Footprint", "Passive" ],
-		'categories': [ "Search Engines" ]
+		'categories': [ "Search Engines" ],
+        'dataSource': {
+            'website': "https://fringeproject.com/",
+            'model': "FREE_NOAUTH_UNLIMITED",
+            'references': [
+                "https://docs.fringeproject.com/"
+            ],
+            'favIcon': "https://fringeproject.com/favicon.png",
+            'logo': "https://fringeproject.com/favicon.png",
+            'description': "Discover the Right Data for Your Security Assessment.\n"
+                               "FringeProject allows you to scan and search through various source of information to discover assets and vulnerabilities.\n"
+                               "We parse your data and add it to our runner. " 
+                               "It does active and passive scanning to find new assets on the Internet.\n",
+        }
 	}
 
     opts = {
