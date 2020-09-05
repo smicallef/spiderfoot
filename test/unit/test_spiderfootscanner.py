a60fda0bffdcecd23848a61c5abf01238540de5b
Brendan Coles <bcoles@gmail.com>
2020-09-05 14:36:07 +0000

    sf: remove unused '__blocknotif' config option

--- test/unit/test_spiderfootscanner.py
+++ test/unit/test_spiderfootscanner.py
@@ -13,7 +13,6 @@ class TestSpiderFootScanner(unittest.TestCase):
         '_debug': False,
         '__logging': True,
         '__outputfilter': None,
-        '__blocknotif': False,
         '_useragent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
         '_dnsserver': '',
         '_fetchtimeout': 5,
