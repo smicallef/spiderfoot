56407c988387a74a9c0105a33532404575009f0b
Brendan Coles <bcoles@gmail.com>
2020-09-20 22:10:19 +0000

    tests: update tests

--- test/unit/modules/test_sfp_countryname.py
+++ test/unit/modules/test_sfp_countryname.py
@@ -54,24 +54,70 @@ class TestModuleCountryName(unittest.TestCase):
         module = sfp_countryname()
         self.assertIsInstance(module.producedEvents(), list)
 
-    def test_handleEvent(self):
-        """
-        Test handleEvent(self, event)
-        """
+    def test_handleEvent_domain_whois_event_data_containing_countryname_string_should_create_country_name_event(self):
         sf = SpiderFoot(self.default_options)
 
         module = sfp_countryname()
         module.setup(sf, dict())
 
-        target_value = 'example target value'
-        target_type = 'IP_ADDRESS'
+        target_value = 'spiderfoot.net'
+        target_type = 'INTERNET_NAME'
         target = SpiderFootTarget(target_value, target_type)
         module.setTarget(target)
+
+        def new_notifyListeners(self, event):
+            expected = 'COUNTRY_NAME'
+            if str(event.eventType) != expected:
+                raise Exception(f"{event.eventType} != {expected}")
+
+            expected = "United States"
+            if str(event.data) != expected:
+                raise Exception(f"{event.data} != {expected}")
+
+        module.notifyListeners = new_notifyListeners.__get__(module, sfp_countryname)
 
         event_type = 'ROOT'
         event_data = 'example data'
         event_module = ''
         source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        event_type = 'DOMAIN_WHOIS'
+        event_data = 'example data 123 Fake St, Fakeville, "United States" example data'
+        event_module = 'example module'
+        source_event = evt
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        result = module.handleEvent(evt)
+
+        self.assertIsNone(result)
+
+    def test_handleEvent_domain_whois_event_data_not_containing_countryname_string_should_not_create_event(self):
+        sf = SpiderFoot(self.default_options)
+
+        module = sfp_countryname()
+        module.setup(sf, dict())
+
+        target_value = 'spiderfoot.net'
+        target_type = 'INTERNET_NAME'
+        target = SpiderFootTarget(target_value, target_type)
+        module.setTarget(target)
+
+        def new_notifyListeners(self, event):
+            raise Exception(f"Raised event {event.eventType}: {event.data}")
+
+        module.notifyListeners = new_notifyListeners.__get__(module, sfp_countryname)
+
+        event_type = 'ROOT'
+        event_data = 'example data'
+        event_module = ''
+        source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        event_type = 'DOMAIN_WHOIS'
+        event_data = 'example data'
+        event_module = 'example module'
+        source_event = evt
         evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
 
         result = module.handleEvent(evt)
--- test/unit/modules/test_sfp_phone.py
+++ test/unit/modules/test_sfp_phone.py
@@ -54,24 +54,108 @@ class TestModulePhone(unittest.TestCase):
         module = sfp_phone()
         self.assertIsInstance(module.producedEvents(), list)
 
-    def test_handleEvent(self):
-        """
-        Test handleEvent(self, event)
-        """
+    def test_handleEvent_domain_whois_event_data_containing_phone_string_should_create_phone_number_event(self):
         sf = SpiderFoot(self.default_options)
 
         module = sfp_phone()
         module.setup(sf, dict())
 
-        target_value = 'example target value'
-        target_type = 'IP_ADDRESS'
+        target_value = 'spiderfoot.net'
+        target_type = 'INTERNET_NAME'
         target = SpiderFootTarget(target_value, target_type)
         module.setTarget(target)
+
+        def new_notifyListeners(self, event):
+            expected = 'PHONE_NUMBER'
+            if str(event.eventType) != expected:
+                raise Exception(f"{event.eventType} != {expected}")
+
+            expected = "+12025550111"
+            if str(event.data) != expected:
+                raise Exception(f"{event.data} != {expected}")
+
+        module.notifyListeners = new_notifyListeners.__get__(module, sfp_phone)
 
         event_type = 'ROOT'
         event_data = 'example data'
         event_module = ''
         source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        event_type = 'DOMAIN_WHOIS'
+        event_data = 'example data +1 202 555 0111 example data'
+        event_module = 'example module'
+        source_event = evt
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        result = module.handleEvent(evt)
+
+        self.assertIsNone(result)
+
+    def test_handleEvent_domain_whois_event_data_not_containing_phone_string_should_not_create_event(self):
+        sf = SpiderFoot(self.default_options)
+
+        module = sfp_phone()
+        module.setup(sf, dict())
+
+        target_value = 'spiderfoot.net'
+        target_type = 'INTERNET_NAME'
+        target = SpiderFootTarget(target_value, target_type)
+        module.setTarget(target)
+
+        def new_notifyListeners(self, event):
+            raise Exception(f"Raised event {event.eventType}: {event.data}")
+
+        module.notifyListeners = new_notifyListeners.__get__(module, sfp_phone)
+
+        event_type = 'ROOT'
+        event_data = 'example data'
+        event_module = ''
+        source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        event_type = 'DOMAIN_WHOIS'
+        event_data = 'example data'
+        event_module = 'example module'
+        source_event = evt
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        result = module.handleEvent(evt)
+
+        self.assertIsNone(result)
+
+    def test_handleEvent_phone_number_event_data_containing_phone_string_should_return_provider_telco_event(self):
+        sf = SpiderFoot(self.default_options)
+
+        module = sfp_phone()
+        module.setup(sf, dict())
+
+        target_value = 'spiderfoot.net'
+        target_type = 'INTERNET_NAME'
+        target = SpiderFootTarget(target_value, target_type)
+        module.setTarget(target)
+
+        def new_notifyListeners(self, event):
+            expected = 'PROVIDER_TELCO'
+            if str(event.eventType) != expected:
+                raise Exception(f"{event.eventType} != {expected}")
+
+            expected = "Swisscom"
+            if str(event.data) != expected:
+                raise Exception(f"{event.data} != {expected}")
+
+        module.notifyListeners = new_notifyListeners.__get__(module, sfp_phone)
+
+        event_type = 'ROOT'
+        event_data = 'example data'
+        event_module = ''
+        source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        event_type = 'PHONE_NUMBER'
+        event_data = '+41798765432'
+        event_module = 'example module'
+        source_event = evt
         evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
 
         result = module.handleEvent(evt)
