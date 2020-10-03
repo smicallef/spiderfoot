9c2003523c556b78cbf3093435101c17c1b1ec33
krishnasism <krishnasis@hotmail.com>
2020-10-03 18:39:54 +0530

    remove unused api key flag

--- modules/sfp_trumail.py
+++ modules/sfp_trumail.py
@@ -20,7 +20,7 @@ class sfp_trumail(SpiderFootPlugin):
     meta = {
         'name': "Trumail",
         'summary': "Check whether an email is disposable",
-        'flags': ["apikey"],
+        'flags': [""],
         'useCases': ["Footprint", "Investigate", "Passive"],
         'categories': ["Search Engines"],
         'dataSource': {
