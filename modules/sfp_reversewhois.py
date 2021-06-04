2f11ffed97b350467e49a28fd869e5afd6d35088
TheTechromancer <thetechromancer@protonmail.com>
2021-06-03 23:13:34 -0400

    copyright

--- modules/sfp_reversewhois.py
+++ modules/sfp_reversewhois.py
@@ -6,6 +6,7 @@
 # Author:      TheTechromancer
 #
 # Created:     05/20/2021
+# Copyright:   (c) Steve Micallef 2021
 # Licence:     GPL
 # -------------------------------------------------------------------------------
 
