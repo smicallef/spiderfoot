5841672437af62150516c30b59eb193d2e40ec99
bcoles <bcoles@gmail.com>
2021-06-13 15:11:57 +1000

    spiderfoot: Use spiderfoot.__version__ for version instead of config dict (#1181)

--- spiderfoot/__init__.py
+++ spiderfoot/__init__.py
@@ -2,3 +2,4 @@ from .db import SpiderFootDb
 from .event import SpiderFootEvent
 from .plugin import SpiderFootPlugin
 from .target import SpiderFootTarget
+from spiderfoot.__version__ import __version__
