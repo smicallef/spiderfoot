66ea21470a076180fedb8d214edfa5e1cb496d71
bcoles <bcoles@gmail.com>
2021-08-24 05:35:37 +1000

    sfp_botvrij: Cleanup (#1289)

--- modules/sfp_botvrij.py
+++ modules/sfp_botvrij.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # -------------------------------------------------------------------------------
 # Name:         sfp_botvrij
-# Purpose:      Checks if a domain is malicious.
+# Purpose:      Check if a domain is malicious according to botvrij.eu.
 #
 # Author:       steve@binarypool.com
 #
@@ -10,20 +10,7 @@
 # Licence:     GPL
 # -------------------------------------------------------------------------------
 
-import re
-
-from netaddr import IPAddress, IPNetwork
-
 from spiderfoot import SpiderFootEvent, SpiderFootPlugin
-
-malchecks = {
-    'botvrij.eu Domain Blocklist': {
-        'id': '_botvrij',
-        'checks': ['domain'],
-        'url': 'https://www.botvrij.eu/data/blocklist/blocklist_full.csv',
-        'regex': '{0},.*'
-    }
-}
 
 
 class sfp_botvrij(SpiderFootPlugin):
@@ -59,122 +46,94 @@ class sfp_botvrij(SpiderFootPlugin):
         'cacheperiod': "Hours to cache list data before re-fetching."
     }
 
-    # Be sure to completely clear any class variables in setup()
-    # or you run the risk of data persisting between scan runs.
-
     results = None
+    errorState = False
 
     def setup(self, sfc, userOpts=dict()):
         self.sf = sfc
         self.results = self.tempStorage()
-
-        # Clear / reset any other class member variables here
-        # or you risk them persisting between threads.
+        self.errorState = False
 
         for opt in list(userOpts.keys()):
             self.opts[opt] = userOpts[opt]
 
     # What events is this module interested in for input
-    # * = be notified about all events.
     def watchedEvents(self):
-        return ["INTERNET_NAME", "IP_ADDRESS",
-                "AFFILIATE_INTERNET_NAME", "AFFILIATE_IPADDR",
-                "CO_HOSTED_SITE"]
+        return [
+            "INTERNET_NAME",
+            "AFFILIATE_INTERNET_NAME",
+            "CO_HOSTED_SITE"
+        ]
 
     # What events this module produces
-    # This is to support the end user in selecting modules based on events
-    # produced.
     def producedEvents(self):
-        return ["MALICIOUS_IPADDR", "MALICIOUS_INTERNET_NAME",
-                "MALICIOUS_AFFILIATE_IPADDR", "MALICIOUS_AFFILIATE_INTERNET_NAME",
-                "MALICIOUS_COHOST"]
+        return [
+            "MALICIOUS_INTERNET_NAME",
+            "MALICIOUS_AFFILIATE_INTERNET_NAME",
+            "MALICIOUS_COHOST"
+        ]
 
-    # Look up 'list' type resources
-    def resourceList(self, id, target, targetType):
-        targetDom = ''
-        # Get the base domain if we're supplied a domain
-        if targetType == "domain":
-            targetDom = self.sf.hostDomain(target, self.opts['_internettlds'])
-            if not targetDom:
-                return None
+    def queryBlacklist(self, target):
+        blacklist = self.retrieveBlacklist()
 
-        for check in list(malchecks.keys()):
-            cid = malchecks[check]['id']
-            if id == cid:
-                data = dict()
-                url = malchecks[check]['url']
-                data['content'] = self.sf.cacheGet("sfmal_" + cid, self.opts.get('cacheperiod', 0))
-                if data['content'] is None:
-                    data = self.sf.fetchUrl(url, timeout=self.opts['_fetchtimeout'], useragent=self.opts['_useragent'])
-                    if data['content'] is None:
-                        self.sf.error("Unable to fetch " + url)
-                        return None
-                    else:
-                        self.sf.cachePut("sfmal_" + cid, data['content'])
+        if target.lower() in blacklist:
+            self.sf.debug(f"Host name {target} found in botvrij.eu blacklist.")
+            return True
 
-                # If we're looking at netblocks
-                if targetType == "netblock":
-                    iplist = list()
-                    # Get the regex, replace {0} with an IP address matcher to
-                    # build a list of IP.
-                    # Cycle through each IP and check if it's in the netblock.
-                    if 'regex' in malchecks[check]:
-                        rx = malchecks[check]['regex'].replace("{0}", r"(\d+\.\d+\.\d+\.\d+)")
-                        pat = re.compile(rx, re.IGNORECASE)
-                        self.sf.debug("New regex for " + check + ": " + rx)
-                        for line in data['content'].split('\n'):
-                            grp = re.findall(pat, line)
-                            if len(grp) > 0:
-                                # self.sf.debug("Adding " + grp[0] + " to list.")
-                                iplist.append(grp[0])
-                    else:
-                        iplist = data['content'].split('\n')
+        return False
 
-                    for ip in iplist:
-                        if len(ip) < 8 or ip.startswith("#"):
-                            continue
-                        ip = ip.strip()
+    def retrieveBlacklist(self):
+        blacklist = self.sf.cacheGet('botvrij', 24)
 
-                        try:
-                            if IPAddress(ip) in IPNetwork(target):
-                                self.sf.debug(f"{ip} found within netblock/subnet {target} in {check}")
-                                return url
-                        except Exception as e:
-                            self.sf.debug(f"Error encountered parsing: {e}")
-                            continue
+        if blacklist is not None:
+            return self.parseBlacklist(blacklist)
 
-                    return None
+        res = self.sf.fetchUrl(
+            "https://www.botvrij.eu/data/blocklist/blocklist_full.csv",
+            timeout=self.opts['_fetchtimeout'],
+            useragent=self.opts['_useragent'],
+        )
 
-                # If we're looking at hostnames/domains/IPs
-                if 'regex' not in malchecks[check]:
-                    for line in data['content'].split('\n'):
-                        if line == target or (targetType == "domain" and line == targetDom):
-                            self.sf.debug(target + "/" + targetDom + " found in " + check + " list.")
-                            return url
-                else:
-                    # Check for the domain and the hostname
-                    try:
-                        rxDom = str(malchecks[check]['regex']).format(targetDom)
-                        rxTgt = str(malchecks[check]['regex']).format(target)
-                        for line in data['content'].split('\n'):
-                            if (targetType == "domain" and re.match(rxDom, line, re.IGNORECASE)) or \
-                                    re.match(rxTgt, line, re.IGNORECASE):
-                                self.sf.debug(target + "/" + targetDom + " found in " + check + " list.")
-                                return url
-                    except Exception as e:
-                        self.sf.debug("Error encountered parsing 2: " + str(e))
-                        continue
+        if res['code'] != "200":
+            self.sf.error(f"Unexpected HTTP response code {res['code']} from botvrij.eu.")
+            self.errorState = True
+            return
 
-        return None
+        if res['content'] is None:
+            self.sf.error("Received no content from botvrij.eu")
+            self.errorState = True
+            return
 
-    def lookupItem(self, resourceId, itemType, target):
-        for check in list(malchecks.keys()):
-            cid = malchecks[check]['id']
-            if cid == resourceId and itemType in malchecks[check]['checks']:
-                self.sf.debug("Checking maliciousness of " + target + " (" + itemType + ") with: " + cid)
-                return self.resourceList(cid, target, itemType)
+        self.sf.cachePut("botvrij", res['content'])
 
-        return None
+        return self.parseBlacklist(res['content'])
+
+    def parseBlacklist(self, blacklist):
+        """Parse plaintext blacklist
+
+        Args:
+            blacklist (str): plaintext blacklist from botvrij.eu
+
+        Returns:
+            list: list of blacklisted host names
+        """
+        hosts = list()
+
+        if not blacklist:
+            return hosts
+
+        for line in blacklist.split('\n'):
+            if not line:
+                continue
+            if line.startswith('#'):
+                continue
+            host = line.strip().split(",")[0].lower()
+            # Note: Validation with sf.validHost() is too slow to use here
+            # if not self.sf.validHost(host, self.opts['_internettlds']):
+            #    continue
+            hosts.append(host)
+
+        return hosts
 
     # Handle events sent to this module
     def handleEvent(self, event):
@@ -186,49 +145,32 @@ class sfp_botvrij(SpiderFootPlugin):
 
         if eventData in self.results:
             self.sf.debug(f"Skipping {eventData}, already checked.")
-            return None
+            return
+
+        if self.errorState:
+            return
 
         self.results[eventData] = True
 
-        if eventName == 'CO_HOSTED_SITE' and not self.opts.get('checkcohosts', False):
-            return None
-        if eventName == 'AFFILIATE_IPADDR' \
-                and not self.opts.get('checkaffiliates', False):
-            return None
+        if eventName == "INTERNET_NAME":
+            evtType = "MALICIOUS_INTERNET_NAME"
+        elif eventName == 'AFFILIATE_INTERNET_NAME':
+            if not self.opts.get('checkaffiliates', False):
+                return
+            evtType = 'MALICIOUS_AFFILIATE_INTERNET_NAME'
+        elif eventName == 'CO_HOSTED_SITE':
+            if not self.opts.get('checkcohosts', False):
+                return
+            evtType = 'MALICIOUS_COHOST'
+        else:
+            return
 
-        for check in list(malchecks.keys()):
-            cid = malchecks[check]['id']
+        self.sf.debug(f"Checking maliciousness of {eventData} ({eventName}) with botvrij.eu")
 
-            if eventName in ['IP_ADDRESS', 'AFFILIATE_IPADDR']:
-                typeId = 'ip'
-                if eventName == 'IP_ADDRESS':
-                    evtType = 'MALICIOUS_IPADDR'
-                else:
-                    evtType = 'MALICIOUS_AFFILIATE_IPADDR'
-
-            if eventName in ['BGP_AS_OWNER', 'BGP_AS_MEMBER']:
-                typeId = 'asn'
-                evtType = 'MALICIOUS_ASN'
-
-            if eventName in ['INTERNET_NAME', 'CO_HOSTED_SITE',
-                             'AFFILIATE_INTERNET_NAME', ]:
-                typeId = 'domain'
-                if eventName == "INTERNET_NAME":
-                    evtType = "MALICIOUS_INTERNET_NAME"
-                if eventName == 'AFFILIATE_INTERNET_NAME':
-                    evtType = 'MALICIOUS_AFFILIATE_INTERNET_NAME'
-                if eventName == 'CO_HOSTED_SITE':
-                    evtType = 'MALICIOUS_COHOST'
-
-            url = self.lookupItem(cid, typeId, eventData)
-
-            if self.checkForStop():
-                return None
-
-            # Notify other modules of what you've found
-            if url is not None:
-                text = f"{check} [{eventData}]\n<SFURL>{url}</SFURL>"
-                evt = SpiderFootEvent(evtType, text, self.__name__, event)
-                self.notifyListeners(evt)
+        if self.queryBlacklist(eventData):
+            url = "https://www.botvrij.eu/data/blocklist/blocklist_full.csv"
+            text = f"botvrij.eu Domain Blocklist [{eventData}]\n<SFURL>{url}</SFURL>"
+            evt = SpiderFootEvent(evtType, text, self.__name__, event)
+            self.notifyListeners(evt)
 
 # End of sfp_botvrij class
