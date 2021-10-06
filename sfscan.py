fb1a93a5e9edb232fd1dcc06aaaeefb9be033ea1
TheTechromancer <thetechromancer@protonmail.com>
2021-10-06 10:43:01 -0400

    Speed up logging by writing log messages to sqlite in batches of 100

--- sfscan.py
+++ sfscan.py
@@ -505,7 +505,7 @@ class SpiderFootScanner():
                     if not mod.errorState and mod.incomingEventQueue is not None:
                         watchedEvents = mod.watchedEvents()
                         if sfEvent.eventType in watchedEvents or "*" in watchedEvents:
-                            mod.incomingEventQueue.put(deepcopy(sfEvent))
+                            mod.incomingEventQueue.put(sfEvent)
 
         except (KeyboardInterrupt, AssertionError) as e:
             self.__sf.status(f"Scan [{self.__scanId}] aborted, {e}.")
