156b1f2783bd4a47dcd0ff04848a3b4982f5835e
bcoles <bcoles@gmail.com>
2021-10-10 16:11:14 +1100

    sfp_yandexdns: Use family filter DNS servers (#1501)

--- test/unit/modules/test_sfp_yandexdns.py
+++ test/unit/modules/test_sfp_yandexdns.py
@@ -34,10 +34,7 @@ class TestModuleyandexdns(unittest.TestCase):
         module = sfp_yandexdns()
         self.assertIsInstance(module.producedEvents(), list)
 
-    def test_queryAddr_should_resolve_unblocked_host(self):
-        """
-        Test queryAddr(self, qaddr)
-        """
+    def test_handleEvent_event_data_safe_internet_name_not_blocked_should_not_return_event(self):
         sf = SpiderFoot(self.default_options)
 
         module = sfp_yandexdns()
@@ -48,22 +45,10 @@ class TestModuleyandexdns(unittest.TestCase):
         target = SpiderFootTarget(target_value, target_type)
         module.setTarget(target)
 
-        resolved = module.queryAddr('yandex.com')
-        self.assertTrue(resolved)
+        def new_notifyListeners(self, event):
+            raise Exception(f"Raised event {event.eventType}: {event.data}")
 
-    def test_handleEvent(self):
-        """
-        Test handleEvent(self, event)
-        """
-        sf = SpiderFoot(self.default_options)
-
-        module = sfp_yandexdns()
-        module.setup(sf, dict())
-
-        target_value = 'example target value'
-        target_type = 'IP_ADDRESS'
-        target = SpiderFootTarget(target_value, target_type)
-        module.setTarget(target)
+        module.notifyListeners = new_notifyListeners.__get__(module, sfp_yandexdns)
 
         event_type = 'ROOT'
         event_data = 'example data'
@@ -71,6 +56,54 @@ class TestModuleyandexdns(unittest.TestCase):
         source_event = ''
         evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
 
+        event_type = 'INTERNET_NAME'
+        event_data = 'yandex.com'
+        event_module = 'example module'
+        source_event = evt
+
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
         result = module.handleEvent(evt)
 
         self.assertIsNone(result)
+
+    def test_handleEvent_event_data_adult_internet_name_blocked_should_return_event(self):
+        sf = SpiderFoot(self.default_options)
+
+        module = sfp_yandexdns()
+        module.setup(sf, dict())
+
+        target_value = 'spiderfoot.net'
+        target_type = 'INTERNET_NAME'
+        target = SpiderFootTarget(target_value, target_type)
+        module.setTarget(target)
+
+        def new_notifyListeners(self, event):
+            expected = 'MALICIOUS_INTERNET_NAME'
+            if str(event.eventType) != expected:
+                raise Exception(f"{event.eventType} != {expected}")
+
+            expected = 'Yandex - Adult [pornhub.com]'
+            if str(event.data) != expected:
+                raise Exception(f"{event.data} != {expected}")
+
+            raise Exception("OK")
+
+        module.notifyListeners = new_notifyListeners.__get__(module, sfp_yandexdns)
+
+        event_type = 'ROOT'
+        event_data = 'example data'
+        event_module = ''
+        source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        event_type = 'INTERNET_NAME'
+        event_data = 'pornhub.com'
+        event_module = 'example module'
+        source_event = evt
+
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        with self.assertRaises(Exception) as cm:
+            module.handleEvent(evt)
+
+        self.assertEqual("OK", str(cm.exception))
