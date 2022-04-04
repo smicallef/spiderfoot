0d9be8e554f798237dc47b4159ab50035d0bdbef
smicallef <steve@binarypool.com>
2022-04-04 22:02:32 +0200

    helpers.py: Add optional additional safe characters.

--- spiderfoot/helpers.py
+++ spiderfoot/helpers.py
@@ -404,11 +404,12 @@ class SpiderFootHelpers():
         return returnArr
 
     @staticmethod
-    def sanitiseInput(cmd: str) -> bool:
+    def sanitiseInput(cmd: str, extra: list = []) -> bool:
         """Verify input command is safe to execute
 
         Args:
             cmd (str): The command to check
+            extra (list): Additional characters to consider safe
 
         Returns:
             bool: command is "safe"
@@ -416,6 +417,10 @@ class SpiderFootHelpers():
         chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.']
+
+        if extra:
+            chars.extend(extra)
+
         for c in cmd:
             if c.lower() not in chars:
                 return False
