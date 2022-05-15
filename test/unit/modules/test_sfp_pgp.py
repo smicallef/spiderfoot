954399f4742788d9b6c566afe488255e32a5140f
bcoles <bcoles@gmail.com>
2022-05-15 23:01:35 +1000

    sfp_pgp: Change default key server to keyserver.ubuntu.com

--- test/unit/modules/test_sfp_pgp.py
+++ test/unit/modules/test_sfp_pgp.py
@@ -3,6 +3,7 @@ import unittest
 
 from modules.sfp_pgp import sfp_pgp
 from sflib import SpiderFoot
+from spiderfoot import SpiderFootEvent, SpiderFootTarget
 
 
 @pytest.mark.usefixtures
@@ -24,3 +25,30 @@ class TestModulePgp(unittest.TestCase):
     def test_producedEvents_should_return_list(self):
         module = sfp_pgp()
         self.assertIsInstance(module.producedEvents(), list)
+
+    def test_handleEvent_no_keyserver_urls_should_set_errorState(self):
+        sf = SpiderFoot(self.default_options)
+
+        module = sfp_pgp()
+        module.setup(sf, dict())
+
+        target_value = 'example target value'
+        target_type = 'IP_ADDRESS'
+        target = SpiderFootTarget(target_value, target_type)
+        module.setTarget(target)
+
+        event_type = 'ROOT'
+        event_data = 'example data'
+        event_module = ''
+        source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        module.opts['keyserver_search1'] = ''
+        module.opts['keyserver_search2'] = ''
+        module.opts['keyserver_fetch1'] = ''
+        module.opts['keyserver_fetch2'] = ''
+
+        result = module.handleEvent(evt)
+
+        self.assertIsNone(result)
+        self.assertTrue(module.errorState)
