d2b0cb3f5d4f26566ea88c54d76c65ce41761097
bcoles <bcoles@gmail.com>
2022-05-30 04:01:42 +1000

    sfp_koodous: Update for new API format with token authentication (#1690)

--- test/unit/modules/test_sfp_koodous.py
+++ test/unit/modules/test_sfp_koodous.py
@@ -3,6 +3,7 @@ import unittest
 
 from modules.sfp_koodous import sfp_koodous
 from sflib import SpiderFoot
+from spiderfoot import SpiderFootEvent, SpiderFootTarget
 
 
 @pytest.mark.usefixtures
@@ -24,3 +25,49 @@ class TestModuleKoodous(unittest.TestCase):
     def test_producedEvents_should_return_list(self):
         module = sfp_koodous()
         self.assertIsInstance(module.producedEvents(), list)
+
+    def test_parseApiResponse_nonfatal_http_response_code_should_not_set_errorState(self):
+        sf = SpiderFoot(self.default_options)
+
+        http_codes = ["200", "404"]
+        for code in http_codes:
+            with self.subTest(code=code):
+                module = sfp_koodous()
+                module.setup(sf, dict())
+                result = module.parseApiResponse({"code": code, "content": None})
+                self.assertIsNone(result)
+                self.assertFalse(module.errorState)
+
+    def test_parseApiResponse_fatal_http_response_error_code_should_set_errorState(self):
+        sf = SpiderFoot(self.default_options)
+
+        http_codes = ["401", "402", "403", "429", "500", "502", "503"]
+        for code in http_codes:
+            with self.subTest(code=code):
+                module = sfp_koodous()
+                module.setup(sf, dict())
+                result = module.parseApiResponse({"code": code, "content": None})
+                self.assertIsNone(result)
+                self.assertTrue(module.errorState)
+
+    def test_handleEvent_no_api_key_should_set_errorState(self):
+        sf = SpiderFoot(self.default_options)
+
+        module = sfp_koodous()
+        module.setup(sf, dict())
+
+        target_value = 'example target value'
+        target_type = 'EMAILADDR'
+        target = SpiderFootTarget(target_value, target_type)
+        module.setTarget(target)
+
+        event_type = 'ROOT'
+        event_data = 'example data'
+        event_module = ''
+        source_event = ''
+        evt = SpiderFootEvent(event_type, event_data, event_module, source_event)
+
+        result = module.handleEvent(evt)
+
+        self.assertIsNone(result)
+        self.assertTrue(module.errorState)
