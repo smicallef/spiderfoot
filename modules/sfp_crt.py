638c71c9c03eac4522c02c51376e1be383e71eeb
bcoles <bcoles@gmail.com>
2022-05-30 04:01:10 +1000

    sfp_crt: Cleanup (#1689)

--- modules/sfp_crt.py
+++ modules/sfp_crt.py
@@ -12,6 +12,7 @@
 # -------------------------------------------------------------------------------
 
 import json
+import time
 import urllib.parse
 
 from spiderfoot import SpiderFootEvent, SpiderFootPlugin
@@ -34,8 +35,7 @@ class sfp_crt(SpiderFootPlugin):
             ],
             'favIcon': "https://crt.sh/sectigo_s.png",
             'logo': "https://crt.sh/sectigo_s.png",
-            'description': "Enter an Identity (Domain Name, Organization Name, etc), "
-            "a Certificate Fingerprint (SHA-1 or SHA-256) or a crt.sh ID",
+            'description': "Free CT Log Certificate Search Tool from Sectigo (formerly Comodo CA)."
         }
     }
 
@@ -51,79 +51,120 @@ class sfp_crt(SpiderFootPlugin):
 
     results = None
     cert_ids = None
+    errorState = False
 
     def setup(self, sfc, userOpts=dict()):
         self.sf = sfc
+        self.errorState = False
         self.results = self.tempStorage()
         self.cert_ids = self.tempStorage()
 
         for opt in list(userOpts.keys()):
             self.opts[opt] = userOpts[opt]
 
-    # What events is this module interested in for input
     def watchedEvents(self):
         return ['DOMAIN_NAME', 'INTERNET_NAME']
 
-    # What events this module produces
-    # This is to support the end user in selecting modules based on events
-    # produced.
     def producedEvents(self):
-        return ["SSL_CERTIFICATE_RAW", "RAW_RIR_DATA",
-                'INTERNET_NAME', 'INTERNET_NAME_UNRESOLVED', 'DOMAIN_NAME',
-                'CO_HOSTED_SITE', 'CO_HOSTED_SITE_DOMAIN']
+        return [
+            "SSL_CERTIFICATE_RAW",
+            "RAW_RIR_DATA",
+            "INTERNET_NAME",
+            "INTERNET_NAME_UNRESOLVED",
+            "DOMAIN_NAME",
+            "CO_HOSTED_SITE",
+            "CO_HOSTED_SITE_DOMAIN"
+        ]
 
-    # Handle events sent to this module
-    def handleEvent(self, event):
-        eventName = event.eventType
-        srcModuleName = event.module
-        eventData = event.data
-
-        if eventData in self.results:
-            return
-
-        self.results[eventData] = True
-
-        self.debug(f"Received event, {eventName}, from {srcModuleName}")
-
+    def queryDomain(self, qry: str):
         params = {
-            'q': '%.' + eventData.encode('raw_unicode_escape').decode("ascii", errors='replace'),
+            'q': '%.' + qry.encode('raw_unicode_escape').decode("ascii", errors='replace'),
             'output': 'json'
         }
 
-        res = self.sf.fetchUrl('https://crt.sh/?' + urllib.parse.urlencode(params),
-                               timeout=30,
-                               useragent=self.opts['_useragent'])
+        res = self.sf.fetchUrl(
+            "https://crt.sh/?" + urllib.parse.urlencode(params),
+            timeout=30,
+            useragent=self.opts['_useragent']
+        )
 
-        if res['content'] is None:
-            self.info("No certificate transparency info found for " + eventData)
-            return
+        time.sleep(0.5)
+
+        return self.parseApiResponse(res)
+
+    def parseApiResponse(self, res: dict):
+        if not res:
+            self.error("No response from crt.sh")
+            return None
+
+        if res['code'] == '404':
+            self.debug("No results for query")
+            return None
+
+        # Future proofing - crt.sh does not implement rate limiting
+        if res['code'] == '429':
+            self.error("You are being rate-limited by crt.sh")
+            self.errorState = True
+            return None
+
+        if res['code'] == '500' or res['code'] == '502' or res['code'] == '503':
+            self.error("crt.sh service is unavailable")
+            self.errorState = True
+            return None
+
+        # Catch all other non-200 status codes, and presume something went wrong
+        if res['code'] != '200':
+            self.error("Failed to retrieve content from crt.sh")
+            self.errorState = True
+            return None
+
+        if not res['content']:
+            return None
 
         try:
-            data = json.loads(res['content'])
+            return json.loads(res['content'])
         except Exception as e:
             self.debug(f"Error processing JSON response: {e}")
+
+        return None
+
+    def handleEvent(self, event):
+        if self.errorState:
             return
 
-        if data is None or len(data) == 0:
+        if event.data in self.results:
+            return
+
+        self.results[event.data] = True
+
+        self.debug(f"Received event, {event.eventType}, from {event.module}")
+
+        data = self.queryDomain(event.data)
+
+        if not data:
+            self.debug(f"No certificate transparency results for domain {event.data}")
             return
 
         evt = SpiderFootEvent("RAW_RIR_DATA", str(data), self.__name__, event)
         self.notifyListeners(evt)
 
         domains = list()
-        fetch_certs = list()
+        certs = list()
 
+        # Extract all domains and certificate IDs from each certificate
         for cert_info in data:
             cert_id = cert_info.get('id')
 
-            if cert_id:
-                # Don't process the same cert twice
-                if cert_id in self.cert_ids:
-                    continue
-                self.cert_ids[cert_id] = True
+            if not cert_id:
+                continue
+
+            if cert_id in self.cert_ids:
+                continue
+
+            self.cert_ids[cert_id] = True
 
             if self.opts['fetchcerts']:
-                fetch_certs.append(cert_id)
+                certs.append(cert_id)
 
             domain = cert_info.get('name_value')
 
@@ -131,9 +172,59 @@ class sfp_crt(SpiderFootPlugin):
                 continue
 
             for d in domain.split("\n"):
-                if d.lower() == eventData.lower():
+                if d.lower() == event.data.lower():
                     continue
                 domains.append(d.lower().replace("*.", ""))
+
+        if self.opts['fetchcerts'] and len(certs) > 0:
+            self.info(f"Retrieving {len(set(certs))} certificates ...")
+
+        # Fetch and store each certificate
+        # Report SAN domains for each certificate
+        for cert_id in set(certs):
+            if self.checkForStop():
+                return
+
+            if self.errorState:
+                break
+
+            params = {
+                'd': str(cert_id)
+            }
+
+            res = self.sf.fetchUrl(
+                'https://crt.sh/?' + urllib.parse.urlencode(params),
+                timeout=30,
+                useragent=self.opts['_useragent']
+            )
+
+            time.sleep(0.5)
+
+            if not res or not res['content']:
+                self.error(f"Error retrieving certificate with ID {cert_id}. No response from crt.sh")
+                continue
+
+            try:
+                cert = self.sf.parseCert(str(res['content']))
+            except Exception as e:
+                self.info(f"Error parsing certificate: {e}")
+                continue
+
+            cert_text = cert.get('text')
+
+            if cert_text:
+                evt = SpiderFootEvent("SSL_CERTIFICATE_RAW", str(cert_text), self.__name__, event)
+                self.notifyListeners(evt)
+
+            sans = cert.get('altnames', list())
+
+            if not sans:
+                continue
+
+            for san in sans:
+                if san.lower() == event.data.lower():
+                    continue
+                domains.append(san.lower().replace("*.", ""))
 
         if self.opts['verify'] and len(domains) > 0:
             self.info(f"Resolving {len(set(domains))} domains ...")
@@ -163,52 +254,5 @@ class sfp_crt(SpiderFootPlugin):
                 else:
                     evt = SpiderFootEvent('DOMAIN_NAME', domain, self.__name__, event)
                     self.notifyListeners(evt)
-
-        for cert_id in fetch_certs:
-            if self.checkForStop():
-                return
-
-            params = {
-                'd': str(cert_id)
-            }
-
-            res = self.sf.fetchUrl('https://crt.sh/?' + urllib.parse.urlencode(params),
-                                   timeout=30,
-                                   useragent=self.opts['_useragent'])
-
-            if res['content'] is None:
-                self.info("Error retrieving certificate with ID " + str(cert_id))
-                continue
-
-            try:
-                cert = self.sf.parseCert(str(res['content']))
-            except Exception as e:
-                self.info('Error parsing certificate: ' + str(e))
-                continue
-
-            evt = SpiderFootEvent("SSL_CERTIFICATE_RAW", cert['text'], self.__name__, event)
-            self.notifyListeners(evt)
-
-            for san in set(cert.get('altnames', list())):
-                domain = san.replace("*.", "")
-
-                if self.getTarget().matches(domain, includeChildren=True):
-                    evt_type = 'INTERNET_NAME'
-                    if self.opts['verify'] and not self.sf.resolveHost(domain):
-                        self.sf.debug("Host " + domain + " could not be resolved")
-                        evt_type += '_UNRESOLVED'
-                else:
-                    evt_type = 'CO_HOSTED_SITE'
-
-                evt = SpiderFootEvent(evt_type, domain, self.__name__, event)
-                self.notifyListeners(evt)
-
-                if self.sf.isDomain(domain, self.opts['_internettlds']):
-                    if evt_type == 'CO_HOSTED_SITE':
-                        evt = SpiderFootEvent('CO_HOSTED_SITE_DOMAIN', domain, self.__name__, event)
-                        self.notifyListeners(evt)
-                    else:
-                        evt = SpiderFootEvent('DOMAIN_NAME', domain, self.__name__, event)
-                        self.notifyListeners(evt)
 
 # End of sfp_crt class
--- test/unit/modules/test_sfp_crt.py
+++ test/unit/modules/test_sfp_crt.py
@@ -24,3 +24,27 @@ class TestModuleCrt(unittest.TestCase):
     def test_producedEvents_should_return_list(self):
         module = sfp_crt()
         self.assertIsInstance(module.producedEvents(), list)
+
+    def test_parseApiResponse_nonfatal_http_response_code_should_not_set_errorState(self):
+        sf = SpiderFoot(self.default_options)
+
+        http_codes = ["200", "404"]
+        for code in http_codes:
+            with self.subTest(code=code):
+                module = sfp_crt()
+                module.setup(sf, dict())
+                result = module.parseApiResponse({"code": code, "content": None})
+                self.assertIsNone(result)
+                self.assertFalse(module.errorState)
+
+    def test_parseApiResponse_fatal_http_response_error_code_should_set_errorState(self):
+        sf = SpiderFoot(self.default_options)
+
+        http_codes = ["401", "403", "429", "500", "502", "503"]
+        for code in http_codes:
+            with self.subTest(code=code):
+                module = sfp_crt()
+                module.setup(sf, dict())
+                result = module.parseApiResponse({"code": code, "content": None})
+                self.assertIsNone(result)
+                self.assertTrue(module.errorState)
