4d8f551006ec880a5c4cc161b685101217f98f98
smicallef <steve@binarypool.com>
2022-12-15 15:39:57 +0100

    sfp_arin: Report HUMAN_NAME directly rather than relying on sfp_names.

--- modules/sfp_arin.py
+++ modules/sfp_arin.py
@@ -71,7 +71,7 @@ class sfp_arin(SpiderFootPlugin):
     # This is to support the end user in selecting modules based on events
     # produced.
     def producedEvents(self):
-        return ["RAW_RIR_DATA"]
+        return ["RAW_RIR_DATA", "HUMAN_NAME"]
 
     # Fetch content and notify of the raw data
     def fetchRir(self, url):
@@ -153,11 +153,7 @@ class sfp_arin(SpiderFootPlugin):
                             sname = name.split(", ", 1)
                             name = sname[1] + " " + sname[0]
 
-                        # A bit of a hack. The reason we do this is because
-                        # the names are separated in the content and sfp_names
-                        # won't recognise it. So we submit this and see if it
-                        # really is considered a name.
-                        evt = SpiderFootEvent("RAW_RIR_DATA", "Possible full name: " + name,
+                        evt = SpiderFootEvent("HUMAN_NAME", name,
                                               self.__name__, self.currentEventSrc)
                         self.notifyListeners(evt)
 
