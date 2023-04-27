3734bbf0abab9e4a46111f1700ce9e0056c33d68
RA80533 <32469082+RA80533@users.noreply.github.com>
2023-04-27 14:55:52 -0400

    Remove `TYPE_CHECKING` check

--- spiderfoot/target.py
+++ spiderfoot/target.py
@@ -4,11 +4,10 @@ import typing
 import netaddr
 
 
-if typing.TYPE_CHECKING:
-    if sys.version_info >= (3, 8):  # PEP 589 support (TypedDict)
-        TargetAlias = typing.TypedDict("TargetAlias", {"type": str, "value": str})
-    else:
-        TargetAlias = typing.Dict[str, str]
+if sys.version_info >= (3, 8):  # PEP 589 support (TypedDict)
+    TargetAlias = typing.TypedDict("TargetAlias", {"type": str, "value": str})
+else:
+    TargetAlias = typing.Dict[str, str]
 
 
 class SpiderFootTarget():
